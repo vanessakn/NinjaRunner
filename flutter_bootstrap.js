3deebf8d6de2cc551d0370665220160d9c45d5b6
KidNationGH <156471218+KidNationGH@users.noreply.github.com>
2026-07-25 05:16:33 +0800

    Deploy Ninja Go direct share preview

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3700399904" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2581391884" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
